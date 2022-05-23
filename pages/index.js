8523b5b8a32f773d27d14c59bb905d5e1778aee6
Rupert Adlmaier <rupert@any.studio>
2022-05-23 11:57:31 +0200

    added placeholder groups in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,34 @@
-import BuddeGroupLogo from '../components/BuddeGroupLogo'
+// import BuddeGroupLogo from '../components/BuddeGroupLogo'
+import Groups from '../components/Groups'
+
+const groups = [
+  {
+    id: 'buddemusic',
+    href: 'https://buddemusic.com',
+    description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+  },
+  {
+    id: 'bta',
+    href: 'https://bta.com/',
+    description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+  },
+  {
+    id: 'phraseddifferently',
+    href: 'http://www.phraseddifferently.com/',
+    description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+  },
+  {
+    id: 'buddemanagement',
+    href: 'https://buddemusicmgmt.com/',
+    description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+  },
+]
 
 export default function Home() {
   return (
     <>
-      <BuddeGroupLogo />
+      {/* <BuddeGroupLogo /> */}
+      <Groups groups={groups} />
     </>
   )
 }

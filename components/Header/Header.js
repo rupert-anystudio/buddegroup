0d425fa32eb915d7d0e38141c0d2e2390a79e3b1
Rupert Adlmaier <rupert@any.studio>
2022-05-23 18:06:13 +0200

    add header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,9 +1,25 @@
 import styled from 'styled-components'
 
-const Header = styled.header`
+const Wrap = styled.header`
   flex: 0 0 auto;
   width: 100%;
   overflow: hidden;
+  background: white;
+  padding: 1rem 1.5rem;
+  margin: 0;
+  border-bottom: 1px solid var(--border-color);
 `
+
+const Logo = styled.div`
+
+`
+
+const Header = () => {
+  return (
+    <Wrap>
+      <Logo>BuddeGroup</Logo>
+    </Wrap>
+  )
+}
 
 export default Header
